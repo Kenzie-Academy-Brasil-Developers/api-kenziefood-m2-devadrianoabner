21276221b6b535d2fc62404acd28786779428f7b
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-03-22 11:55:30 -0300

    Fix/Api.js

--- src/js/api/Api.js
+++ src/js/api/Api.js
@@ -4,32 +4,32 @@ class Api {
 
     static API_URL = "https://kenzie-food-api.herokuapp.com/"
 
-    static async getProduts(){
+    static async getProduts() {
         const response = await fetch(`${this.API_URL}products`)
-        const responseData = response.json()    
+        const responseData = response.json()
         return responseData
     }
-    
-    static async register(data){
+
+    static async register(data) {
         const msgErro = document.querySelector('.msgBadError')
-        const response =  await fetch(`${this.API_URL}auth/register`,{
+        const response = await fetch(`${this.API_URL}auth/register`, {
             "method": "POST",
             "headers": {
                 "Content-Type": "application/json"
             },
             "body": JSON.stringify(data)
         })
-        const responseData =  response.json()
 
-        switch(responseData.status){
-            case 201:
+        switch (response.status) {
+            case 200:
                 msgErro.innerText = ''
                 window.location.replace("./Login.html")
-            break;
+                break;
             default:
                 msgErro.innerText = 'Ops, aconteceu algo de errado, tente novamente.'
         }
         console.log(responseData)
+        const responseData = response.json()
         return responseData
     }
 }
