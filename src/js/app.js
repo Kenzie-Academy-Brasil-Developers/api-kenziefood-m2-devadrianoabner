940d1df6b57fd319bb983d1a5cf5cb5081d27191
adriano abner <adriano.abner@live.com>
2022-03-22 19:40:13 -0300

    métodos de criar e enviar cards criados

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import Api from "./api/Api.js";
+import { Controller } from "./Controller/controller.js";
 
 const productsList = await Api.getProduts()
+
+Controller.sendProductsCards(productsList)
 
 console.log(productsList)
 
