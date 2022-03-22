10a5fdc80f07601251aef71a2c679fa7c8b03ea3
PVillor <95309092+PVillor@users.noreply.github.com>
2022-03-22 15:43:14 -0300

    feat/Filter

---
+++ src/js/models/Filter.js
@@ -0,0 +1,30 @@
+export class Filter {
+
+    //CATEGORIA
+    static filterCategory(products, btn) {
+
+        let category = btn.value
+
+        const productsFilt = products.filter(
+            (product) => product.categoria === category
+          )
+          return productsFilt
+    }
+  
+    //NOME
+    static filterSearchLabel(products, inputValue) {
+        const inputSearch = inputValue
+
+        const productsFilt = products.filter((product) => {
+          if (
+            product.name.includes(inputSearch) ||
+            product.section.includes(inputSearch) ||
+            product.category.includes(inputSearch)
+          ) {
+            return true
+          }
+        })
+    
+        return productsFilt
+    }
+}
