03192d8d8811db9d59260e0ba186f1339bf427be
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-03-22 08:52:06 -0300

    Feat/Api.js

--- src/js/api/Api.js
+++ src/js/api/Api.js
@@ -2,7 +2,6 @@ class Api {
 
     static API_URL = "https://kenzie-food-api.herokuapp.com/"
 
-
     static async getProduts(){
         const response = await fetch(`${this.API_URL}products`)
         const responseData = response.json()    
