ea203899f5e026eb5e4d19c097c92b46acb504ed
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-03-23 19:15:12 -0300

    feat/templateStatus

--- src/js/controllers/Status.js
+++ src/js/controllers/Status.js
@@ -1,7 +1,28 @@
 class Status {
 
-        static main = 
-    static templateYes(name){
+    static main = document.querySelector('.content__status')
+    static templateYes(name) {
+        this.main.innerHTML = `
+         <div class='content__status--element'>
+                <div class='content__status--elementTitle'>  
+                    <h2 class='content__status-title'>Status</h2>
+                </div>
+                <p class='content__status-text'>${name}</p>
+                <div class='content__status-colorGreen'></div>
+         </div>
+        `
+    }
 
+    static templateNo(name) {
+        this.main.innerHTML = `
+        
+                <div class='content__status--element'>
+                <div class='content__status--elementTitle'>  
+                <h2 class='content__status-title'>Status</h2>
+                </div>
+                <p class='content__status-text'>${name}</p>
+                <div class='content__status-colorRed'></div>
+            </div>    
+        `
     }
 }
