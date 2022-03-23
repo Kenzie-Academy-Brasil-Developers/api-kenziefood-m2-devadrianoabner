886df6c5fec0c66f9d7b3435d3ed1f2039a2d2c8
adriano abner <adriano.abner@live.com>
2022-03-22 23:47:19 -0300

    começando a fazer o carrinho

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,17 @@ import { Filter } from "./models/Filter.js"
 
 const productsList = await Api.getProduts()
 Controller.sendProductsCards(productsList)
+console.log(productsList)
+const sectionOfProducts = document.querySelector(".product__Cards")
 
 
 const filtAll = document.querySelector(".filt__all")
 filtAll.addEventListener("click", () => {
+
+    
+    sectionOfProducts.innerHTML = ""
+    Controller.sendProductsCards(productsList)
+
 
     console.log(productsList)
     //paginaDeProdutos.listarProdutos(productsList)
@@ -17,6 +24,10 @@ const filtPan = document.querySelector(".filt__pan")
 filtPan.addEventListener("click", () => {
 
     const result = Filter.filterCategory(productsList, filtPan)
+sectionOfProducts.innerHTML = ""
+    
+    Controller.sendProductsCards(result)    
+
     console.log(result)
 
     //paginaDeProdutos.listarProdutos(result)
@@ -26,6 +37,9 @@ const filtFruits = document.querySelector(".filt__fruits")
 filtFruits.addEventListener("click", () => {
 
     const result = Filter.filterCategory(productsList, filtFruits)
+    sectionOfProducts.innerHTML = ""
+    
+    Controller.sendProductsCards(result)
     console.log(result)
 
     //paginaDeProdutos.listarProdutos(result)
@@ -33,8 +47,11 @@ filtFruits.addEventListener("click", () => {
 
 const filtDrinks = document.querySelector(".filt__drinks")
 filtDrinks.addEventListener("click", () => {
-
+    
     const result = Filter.filterCategory(productsList, filtDrinks)
+    sectionOfProducts.innerHTML = ""
+    
+    Controller.sendProductsCards(result)
     console.log(result)
 
     //paginaDeProdutos.listarProdutos(result)
@@ -45,8 +62,16 @@ searchLabel.addEventListener("keyup", () => {
     
     const value = searchLabel.value
     const result = Filter.filterSearchLabel(productsList, value)
+    sectionOfProducts.innerHTML = ""
+    
+    Controller.sendProductsCards(result)
     console.log(result)
 
     //paginaDeProdutos.listarProdutos(result)
 })
 
+sectionOfProducts.addEventListener("click", (event) =>{
+    
+   let productId = event.target.id 
+   
+})
